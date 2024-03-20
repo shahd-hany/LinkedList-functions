4795f32ca239b326806688b72dc0a1b1b2a9af7a
shahd-hany <sh1904@fayoum.edu.eg>
2024-03-20 19:07:08 +0200

    1st version of android app

---
+++ linkedList/functionOccurence.cpp
@@ -0,0 +1,29 @@
+﻿/*time complexity for this function O(n ^2) where "n" is number of nudes that is the head of linked list in first loop as its worst case and "n" number of nudes in second loop
+	 as linked list might not contain the element as its worst case as well  */
+	 // space  complexity is O(1) there is no new space in memory needed as i use temp pointers only . 
+void occurrences(node*& head, int elemnt) {
+	//هنغير في مكان ال head عشان ممكن ال element يبقا في اول الليست فلازم نشيله
+	while (head && (head->data == elemnt)) {
+		node* temp = head;
+		//هشاور بيه على ال head وهحرك ال head لل node اللي بعده
+		head = head->next;
+		delete temp;
+		//همسح ال temp اللي كان اصلا ال head بس حركنا ال head ومسحنا محتواه القديم
+	}
+	node* temp = head;
+	//همشي ال Temp على ال Linked list كلها ومينفعش احرك ال head
+	while (temp && temp->next) {
+		if (temp->next->data == elemnt) {
+			//مش هحركه من مكانه وهبص على اللي بعده 
+			node* t = temp->next;
+			//هكريت بوينتر عشان يشاور عليه ويمسحه
+			temp->next = temp->next->next;
+			//هخليه يسكيب ال node اللي فيها ال elemnt
+			delete t;
+			// هنمسح البيونتر اللي بيشاور على ال node اللي فيها ال elemnt
+		}
+		else
+			//لو ال node اللي بعده امان هيتحرك من مكانه ليها
+			temp = temp->next;
+	}
+}
---
+++ linkedList/functionSum.cpp
@@ -0,0 +1,11 @@
+﻿//time complexity for this function o(n) where n is number of nudes 
+	// space  complexity is o(n) each recursive call to sum function consumes additional space on the stack to store its variables and return address.
+int sum(node* ptr) {
+	//هناخد ال head arg ونتحرك من بعده
+	if (ptr == nullptr)
+		//ال base عشان الفانكشن تقف وال tree تخلص ويبدا يجمع مفكوك كل فانكشن
+		return 0;
+	return ptr->data + sum(ptr->next);
+	//هيجمع داتا الnode وياخد اللي بعدها argument عشان ياخد الداتا بتاعتها
+
+}
